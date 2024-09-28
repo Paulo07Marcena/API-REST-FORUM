4f6febcb528e66dd4007a64bd2a5c72bfa079f1c
paulo07marcena <paulo.marcena@sptech.school>
2024-09-28 20:52:34 -0300

    feat: Adding new DTOs

--- src/main/kotlin/br/com/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/forum/controller/TopicoController.kt
@@ -1,17 +1,13 @@
 package br.com.forum.controller
 
-import br.com.forum.dto.NovoTopicoDto
-import br.com.forum.model.Curso
-import br.com.forum.model.Topico
-import br.com.forum.model.Usuario
+import br.com.forum.dto.NovoTopicoForm
+import br.com.forum.dto.TopicoView
 import br.com.forum.service.TopicoService
-import jakarta.websocket.server.PathParam
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -21,17 +17,17 @@ class TopicoController(
 ) {
 
     @GetMapping
-    fun listar(): List<Topico> {
+    fun listar(): List<TopicoView> {
         return service.listar();
     }
 
     @GetMapping("/{id}")
-    fun buscarPorId(@PathVariable id: Long): Topico {
+    fun buscarPorId(@PathVariable id: Long): TopicoView {
         return service.buscarPorId(id);
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody dto: NovoTopicoDto) {
+    fun cadastrar(@RequestBody dto: NovoTopicoForm) {
         service.cadastrar(dto)
     }
 }
--- src/main/kotlin/br/com/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/forum/service/TopicoService.kt
@@ -1,8 +1,10 @@
 package br.com.forum.service
 
-import br.com.forum.dto.NovoTopicoDto
+import br.com.forum.dto.NovoTopicoForm
+import br.com.forum.dto.TopicoView
 import br.com.forum.model.Topico
 import org.springframework.stereotype.Service
+import java.util.stream.Collectors
 
 @Service
 class TopicoService(
@@ -11,19 +13,33 @@ class TopicoService(
     private var usuarioService: UsuarioService
 ) {
 
-    fun listar(): List<Topico> {
-        return topicos
+    fun listar(): List<TopicoView> {
+        return topicos.stream().map{ t -> TopicoView(
+            id = t.id,
+            titulo = t.titulo,
+            mensagem = t.mensagem,
+            status = t.status,
+            dataCriacao = t.dataCriacao
+        ) }.collect(Collectors.toList())
     }
 
-    fun buscarPorId(id: Long): Topico {
-        return topicos
+    fun buscarPorId(id: Long): TopicoView {
+        var topico = topicos
             .stream()
             .filter(
                 { t -> t.id == id }
             ).findFirst().get()
+
+        return TopicoView(
+            id = topico.id,
+            titulo = topico.titulo,
+            mensagem = topico.mensagem,
+            status = topico.status,
+            dataCriacao = topico.dataCriacao
+        )
     }
 
-    fun cadastrar(dto: NovoTopicoDto) {
+    fun cadastrar(dto: NovoTopicoForm) {
         topicos = topicos.plus(
             Topico(
                 id = topicos.size.toLong() + 1,
