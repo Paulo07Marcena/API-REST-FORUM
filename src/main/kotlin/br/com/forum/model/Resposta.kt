f18216dc24a1440ebdc0f11194f530fd29865020
paulo07marcena <paulo.marcena@sptech.school>
2024-10-01 17:00:12 -0300

    feat: Transforming models in entities

--- src/main/kotlin/br/com/forum/model/Resposta.kt
+++ src/main/kotlin/br/com/forum/model/Resposta.kt
@@ -1,9 +1,19 @@
 package br.com.forum.model
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+
+@Entity
 data class Resposta (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val mensagem: String,
+    @ManyToOne
     val topico: Topico,
+    @ManyToOne
     val autor: Usuario,
     val solucao: Boolean
 )
