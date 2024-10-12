9fe22c63f7a875918ef974ed8c574368ebb9180f
paulo07marcena <paulo.marcena@sptech.school>
2024-10-12 10:44:05 -0300

    feat: Adding JWT configuration at SecurityConfiguration Kotlin class

--- src/main/kotlin/br/com/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/forum/config/SecurityConfiguration.kt
@@ -3,35 +3,41 @@ package br.com.forum.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.security.config.Customizer
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration(
+    private val jwtUtil: JWTUtil,
+    private val authenticationManagerBuilder: AuthenticationManagerBuilder
+) {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .authorizeHttpRequests {
-                it.requestMatchers(HttpMethod.GET, "/topicos").hasAuthority("LEITURA_ESCRITA")
-                    it.anyRequest().authenticated()
+            .authorizeHttpRequests { requests ->
+                requests
+                    //.requestMatchers(HttpMethod.GET, "/topicos").hasAuthority("LEITURA_ESCRITA")
+                    .requestMatchers("/login").permitAll()
+                    .anyRequest().authenticated()
             }
+            .addFilterBefore(JWTLoginFilter(authenticationManager = authenticationManagerBuilder, jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter::class.java)
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
-            .formLogin().disable().httpBasic()
+            .formLogin().disable()
+            .httpBasic()
 
         return http.build()
     }
 
     @Bean
-    fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
-
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
